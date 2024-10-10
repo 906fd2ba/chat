5fa08698827a9db5b491f9e13a4cd43fc6f05e0b
roma <->
2024-10-10 09:39:31 +0700

    no store

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script setup>
 import User from "@/entities/User.js";
-import Chat from "@/components/Chat.vue";
+import ChatFeed from "@/components/ChatFeed.vue";
+import ChatControls from "@/components/ChatControls.vue";
+import useChat from "@/composables/useChat.js";
 
 const userList = [
   new User('user1'),
   new User('user2'),
 ];
+
+const { messages, addMessage } = useChat();
 </script>
 
 <template>
   <div class="container">
-    <Chat v-for="user of userList" :key="user.id" :user="user" />
+    <div class="chat" v-for="user of userList" :key="user.id">
+      <div class="chat-user">Чат от лица {{ user.name }}</div>
+
+      <ChatFeed :user="user" :messages="messages" />
+
+      <ChatControls @sendMessage="addMessage({ message: $event, user })" />
+    </div>
   </div>
 </template>
 
@@ -18,5 +28,9 @@ const userList = [
 .container {
   display: flex;
   justify-content: space-evenly;
+}
+
+.chat-user {
+  margin-bottom: 20px;
 }
 </style>
